68fb44534bf74b693f4b087f42f9fb6da982eec9
takahiro tada <t.tada@kms3.com>
2024-09-30 19:43:14 +0900

    add api.

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return "DAISUKE is here!"
 
-
-@app.get("/chat/test")
+@app.post("/chat/test")
 def read_root():
 
     with open('secret.json', encoding="utf-8") as f:
@@ -23,50 +22,17 @@ def read_root():
         api_key=secret['api_key'],
     )
 
-    stream = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[{"role": "user", "content": "Say this is a test"}],
-        stream=True,
-    )
+    def generate():
+        stream = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "user", "content": "C#でfizz buzzを作ってください"}
+            ],
+            stream=True,
+        )
 
-    return StreamingResponse(stream)
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                yield chunk.choices[0].delta.content
 
-    #for chunk in stream:
-    #    if chunk.choices[0].delta.content is not None:
-    #        print(chunk.choices[0].delta.content, end="")
-#
-    #return {"Hello": "World"}
-
-"""
-with open('secret.json', encoding="utf-8") as f:
-    secret = json.load(f)
-
-# クライアントを作成
-client = OpenAI(
-    organization=secret['organization'],
-    project=secret['project'],
-    api_key=secret['api_key'],
-)
-
-stream = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{"role": "user", "content": "Say this is a test"}],
-    stream=True,
-)
-
-for chunk in stream:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
-"""
-        
-"""
-RESPONSE = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "私はあなたのアシスタントです。"},
-        {"role": "user", "content": "こんにちは！"},
-    ]
-)
-
-print(RESPONSE)
-"""
+    return StreamingResponse(generate(), media_type="text/plain")
