ebd7ce9f0cfbfaa4fd1f61c20edf14e10df09520
TAKAHIRO TADA <takass0119@yahoo.co.jp>
2024-10-12 00:30:31 +0900

    add docker build.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from asyncio import sleep
+import os
 import json
 from openai import OpenAI
 from pydantic import BaseModel
@@ -25,8 +25,33 @@ app.add_middleware(
 
 user_id = 1
 
-with open('secret.json', encoding="utf-8") as f:
-    secret = json.load(f)
+secret = {
+    "organization": "org-xxxxxx",
+    "project": "proj-xxxxxx",
+    "api_key": "sk-xxxxxx",
+    "db_user": "root",
+    "db_host": "localhost",
+    "db_password": "password",
+    "db_port": 3306,
+    "db_name": "chat"
+}
+
+secret['organization'] = os.getenv('ORGANIZATION', secret['organization'])
+secret['project'] = os.getenv('PROJECT', secret['project'])
+secret['api_key'] = os.getenv('API_KEY', secret['api_key'])
+secret['db_user'] = os.getenv('DB_USER', secret['db_user'])
+secret['db_host'] = os.getenv('DB_HOST', secret['db_host'])
+secret['db_password'] = os.getenv('DB_PASSWORD', secret['db_password'])
+secret['db_port'] = os.getenv('DB_PORT', secret['db_port'])
+secret['db_name'] = os.getenv('DB_NAME', secret['db_name'])
+
+conn_params = {
+    "user": secret['db_user'],
+    "password": secret['db_password'],
+    "host": secret['db_host'],
+    "port": int(secret['db_port']),
+    "database": secret['db_name']
+}
 
 openai = OpenAI(
     organization=secret['organization'],
@@ -51,13 +76,7 @@ def create_conversation():
     """
     新しい会話を始めるためのエンドポイント
     """
-    with mariadb.connect(
-        user=secret['db_user'],
-        password=secret['db_password'],
-        host=secret['db_host'],
-        port=secret['db_port'],
-        database=secret['db_name']
-    ) as conn:
+    with mariadb.connect(**conn_params) as conn:
 
         with conn.cursor() as cur:
             cur.execute(
@@ -78,14 +97,7 @@ def post_message(gpt_request: GptRequest):
     """
     会話でメッセージを送信するためのエンドポイント
     """
-
-    with mariadb.connect(
-        user=secret['db_user'],
-        password=secret['db_password'],
-        host=secret['db_host'],
-        port=secret['db_port'],
-        database=secret['db_name']
-    ) as conn:
+    with mariadb.connect(**conn_params) as conn:
 
         with conn.cursor() as cur:
             conversation_id = get_conversation_id(user_id, cur)
@@ -113,14 +125,7 @@ def post_message(gpt_request: GptRequest):
     """
     会話でメッセージを送信するためのエンドポイント
     """
-
-    with mariadb.connect(
-        user=secret['db_user'],
-        password=secret['db_password'],
-        host=secret['db_host'],
-        port=secret['db_port'],
-        database=secret['db_name']
-    ) as conn:
+    with mariadb.connect(**conn_params) as conn:
 
         with conn.cursor() as cur:
             conversation_id = get_conversation_id(user_id, cur)
@@ -160,13 +165,7 @@ async def generate(model:str, conversation_id:int, messages:list, prompt:str):
         content_full += content
         yield content
 
-    with mariadb.connect(
-        user=secret['db_user'],
-        password=secret['db_password'],
-        host=secret['db_host'],
-        port=secret['db_port'],
-        database=secret['db_name']
-    ) as conn:
+    with mariadb.connect(**conn_params) as conn:
 
         with conn.cursor() as cur:
 
@@ -199,244 +198,3 @@ def get_conversation_id(user_id : int, cur : mariadb.Cursor) -> int:
 
     for (id) in cur:
         return id[0]
-
-
-# @app.get("/")
-# def read_root():
-#     """
-#     DAISUKEはここにいるよ！
-#     """
-#     return "DAISUKE is here!"
-# 
-# @app.post("/test/gpt")
-# def test_gpt(gpt_request: GptRequest):
-#     """
-#     GPTモデルのテスト用エンドポイント
-#     """
-# 
-#     with open('secret.json', encoding="utf-8") as f:
-#         secret = json.load(f)
-# 
-#     openai = OpenAI(
-#         organization=secret['organization'],
-#         project=secret['project'],
-#         api_key=secret['api_key'],
-#     )
-# 
-#     def generate():
-#         stream = openai.chat.completions.create(
-#             model="gpt-4o-mini",
-#             messages=[
-#                 {"role": "user", "content": gpt_request.prompt}
-#             ],
-#             stream=True,
-#         )
-# 
-#         content_full = ''
-#         for chunk in stream:
-#             content = chunk.choices[0].delta.content
-#             if content is None:
-#                 continue
-#             content_full += content
-#             yield content
-# 
-#         with mariadb.connect(
-#             user=secret['db_user'],
-#             password=secret['db_password'],
-#             host=secret['db_host'],
-#             port=secret['db_port'],
-#             database=secret['db_name']
-#         ) as conn:
-# 
-#             with conn.cursor() as cur:
-#                 conversation_id = 1
-#                 cur.execute(
-#                     """
-#                     INSERT INTO conversation_posts (conversation_id, `role`, message) 
-#                     VALUES(?, 'User', ?);
-#                     """,
-#                     (conversation_id, gpt_request.prompt))
-#                 cur.execute(
-#                     """
-#                     INSERT INTO conversation_posts (conversation_id, `role`, message) 
-#                     VALUES(?, 'Assistant', ?);
-#                     """,
-#                     (conversation_id, content_full))
-# 
-#                 cur.execute('COMMIT')
-# 
-#     return StreamingResponse(generate(), media_type="text/plain")
-# 
-# 
-# @app.post("/test/reasoning")
-# def test_reasoning():
-#     """
-#     推論モデルのテスト用エンドポイント
-#     """
-# 
-#     with open('secret.json', encoding="utf-8") as f:
-#         secret = json.load(f)
-# 
-#     openai = OpenAI(
-#         organization=secret['organization'],
-#         project=secret['project'],
-#         api_key=secret['api_key'],
-#     )
-# 
-#     response = openai.chat.completions.create(
-#         model="o1-preview",
-#         messages=[
-#             {
-#                 "role": "user", 
-#                 "content": "'[1,2],[3,4],[5,6]' 形式の文字列として表される行列を受け取り、同じ形式で転置結果を出力する bash スクリプトを作成します。"
-#             }
-#         ]
-#     )
-# 
-#     return response.choices[0].message.content
-# 
-# @app.post("/test/image")
-# def test_image(dalle_request: DalleRequest):
-#     """
-#     画像生成のテスト用エンドポイント
-#     """
-# 
-#     with open('secret.json', encoding="utf-8") as f:
-#         secret = json.load(f)
-# 
-#     openai = OpenAI(
-#         organization=secret['organization'],
-#         project=secret['project'],
-#         api_key=secret['api_key'],
-#     )
-# 
-#     response = openai.images.generate(
-#         model="dall-e-3",
-#         prompt=dalle_request.prompt,
-#         size="1024x1024",
-#         quality="standard",
-#         n=1,
-#     )
-# 
-#     return response.data[0].url
-# 
-# 
-# @app.post("/test/sse")
-# async def test_server_send_event():
-#     """
-#     Server-Sent Eventsのテスト用エンドポイント
-#     """
-# 
-#     async def waypoints_generator():
-#         waypoints = open('waypoints.json', encoding="utf-8")
-#         waypoints = json.load(waypoints)
-#         for waypoint in waypoints[0: 10]:
-#             data = json.dumps(waypoint)
-#             yield f"event: locationUpdate\ndata: {data}\n\n"
-#             await sleep(1)
-# 
-#     return StreamingResponse(waypoints_generator(), media_type="text/event-stream")
-# 
-# @app.post("/test/sse_gpt")
-# async def test_server_send_event_gpt(gpt_request: GptRequest):
-#     """
-#     GPTの回答をServer-Sent Eventsでストリーミングするテスト用エンドポイント
-#     """
-# 
-#     with open('secret.json', encoding="utf-8") as f:
-#         secret = json.load(f)
-# 
-#     openai = OpenAI(
-#         organization=secret['organization'],
-#         project=secret['project'],
-#         api_key=secret['api_key'],
-#     )
-# 
-#     async def generate():
-#         stream = openai.chat.completions.create(
-#             model="gpt-4o-mini",
-#             messages=[
-#                 {"role": "user", "content": gpt_request.prompt}
-#             ],
-#             stream=True,
-#         )
-# 
-#         content_full = ''
-#         for chunk in stream:
-#             content = chunk.choices[0].delta.content
-#             if content is None:
-#                 continue
-#             content_full += content
-#             yield content
-# 
-#         with mariadb.connect(
-#             user=secret['db_user'],
-#             password=secret['db_password'],
-#             host=secret['db_host'],
-#             port=secret['db_port'],
-#             database=secret['db_name']
-#         ) as conn:
-# 
-#             with conn.cursor() as cur:
-#                 conversation_id = 1
-#                 cur.execute(
-#                     """
-#                     INSERT INTO conversation_posts (conversation_id, `role`, message) 
-#                     VALUES(?, 'User', ?);
-#                     """,
-#                     (conversation_id, gpt_request.prompt))
-#                 cur.execute(
-#                     """
-#                     INSERT INTO conversation_posts (conversation_id, `role`, message) 
-#                     VALUES(?, 'Assistant', ?);
-#                     """,
-#                     (conversation_id, content_full))
-# 
-#                 cur.execute('COMMIT')
-# 
-#     return EventSourceResponse(generate())
-# 
-# 
-# @app.post("/test/mariadb")
-# async def test_mariadb():
-#     """
-#     Maria DBのテスト用エンドポイント
-#     """
-#     with open('secret.json', encoding="utf-8") as f:
-#         secret = json.load(f)
-# 
-#     with mariadb.connect(
-#         user=secret['db_user'],
-#         password=secret['db_password'],
-#         host=secret['db_host'],
-#         port=secret['db_port'],
-#         database=secret['db_name']
-#     ) as conn:
-# 
-#         with conn.cursor() as cur:
-# 
-#             conversation_id = 1
-#             cur.execute(
-#                 """
-#                 INSERT INTO conversation_posts (conversation_id, `role`, message) 
-#                 VALUES(?, 'User', 'こんにちは。');
-#                 """,
-#                 (conversation_id))
-# 
-#             cur.execute('COMMIT')
-# 
-#             cur.execute(
-#                 """
-#                 SELECT role, message 
-#                 FROM conversation_posts
-#                 WHERE conversation_id = ?
-#                 ORDER BY posted_at
-#                 """ ,
-#                 (conversation_id,))
-# 
-#             result = ""
-#             for (role, message) in cur:
-#                 result += f"{role}: {message}\n"
-# 
-#     return result
-# 
